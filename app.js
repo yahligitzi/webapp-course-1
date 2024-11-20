c58282f97e4dd489380fa2f15ec825301d7a6f29
inbal <inbalbarak123@gmail.com>
2024-11-20 11:25:16 +0200

    initualize mongodb

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Connected to database");
+});
+
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
